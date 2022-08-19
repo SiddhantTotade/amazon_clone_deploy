318bf127e9171f31247bb93f1e49b9c13bf3bd07
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-08-20 01:13:28 +0530

    changes urls

--- eshop/urls.py
+++ eshop/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
